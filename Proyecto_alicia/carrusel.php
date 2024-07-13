a5ad36b2aa05723dd66895b8a35d4706bbf20df4
Cristian-Vranic <75226003+Wiqness@users.noreply.github.com>
2024-07-12 22:15:27 -0300

    Add files via upload

---
+++ Proyecto_alicia/carrusel.php
@@ -0,0 +1,75 @@
+<!-- data-bs-interval nos permite establecer el intervalo de tiempo para cada imagen del carrousel -->
+<!-- data-bs-ride="carousel" permite que el carousel se reproduzca automaticamente  -->
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0"
+            class="active button_carousel" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" class="button_carousel"
+            aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" class="button_carousel"
+            aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" class="button_carousel"
+            aria-label="Slide 4"></button> <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" class="button_carousel"
+            aria-label="Slide 5"></button> <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5" class="button_carousel"
+            aria-label="Slide 6"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="4000">
+            <img src="res/img/1-Pronunciamientos-.png" class="d-block w-100 imagen_1 imagen" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <!-- <h5>First slide label</h5>
+                <p>Some representative placeholder content for the first slide.</p> -->
+            </div>
+        </div>
+        <div class="carousel-item" data-bs-interval="4000">
+            <img src="res/img/2-Inscripcion-a-carreras.png" class="d-block w-100 imagen_2 imagen" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <!-- <h5>Second slide label</h5>
+                <p>Some representative placeholder content for the second slide.</p> -->
+            </div>
+        </div>
+        <div class="carousel-item" data-bs-interval="4000">
+
+            <img src="res/img/3-Acceso-al-siu-guarani-.png" class="d-block w-100 imagen_3 imagen" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <!-- <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p> -->
+            </div>
+        </div>
+
+        <div class="carousel-item" data-bs-interval="4000">
+
+            <img src="res/img/4-jORNADAS-EN-EL-ALICIA-B.png" class="d-block w-100 imagen_3 imagen" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <!-- <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p> -->
+            </div>
+        </div>
+
+        <div class="carousel-item" data-bs-interval="4000">
+
+            <img src="res/img/5-PROYECTOCREACION.png" class="d-block w-100 imagen_3 imagen" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <!-- <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p> -->
+            </div>
+        </div>
+
+        <div class="carousel-item" data-bs-interval="4000">
+
+            <img src="res/img/6-Sitio-web-de-tutoria.png" class="d-block w-100 imagen_3 imagen" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <!-- <h5>Third slide label</h5>
+                <p>Some representative placeholder content for the third slide.</p> -->
+            </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
---
+++ Proyecto_alicia/navbar.php
@@ -0,0 +1,121 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid navegador">
+        <a class="navbar-brand" href="#"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        <div class="collapse navbar-collapse navbar_principal" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            EL INSTITUTO
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Datos institucionales</a></li>
+                            <li><a class="dropdown-item" href="#">Autoridades</a></li>
+                            <li><a class="dropdown-item" href="#">Consejo directivo ></a></li>
+                            <li><a class="dropdown-item" href="#">Coordinadores y Directores</a></li>
+                            <li><a class="dropdown-item" href="#">Historia</a></li>
+                            <li><a class="dropdown-item" href="#">Institucional Departamento de alumnos 2023</a></li>
+                            <li><a class="dropdown-item" href="#">Reglamento Org&aacute;nico</a></li>
+                            <li><a class="dropdown-item" href="#">Juntas Departamentales ></a></li>
+                            <li><a class="dropdown-item" href="#">Cronograma 2024</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            ESTUDIANTES
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Horarios 2024</a></li>
+                            <li><a class="dropdown-item" href="#">Formulario apto psicof&iacute;sico</a></li>
+                            <li><a class="dropdown-item" href="#">Tutorias y orientacion al estudiante</a></li>
+                            <li><a class="dropdown-item" href="#">Pares-tutores</a></li>
+                            <li><a class="dropdown-item" href="#">Becas vigentes</a></li>
+                            <li><a class="dropdown-item" href="#">Formularios varios</a></li>
+                            <li><a class="dropdown-item" href="#">Consultas al depto. de inform&aacute;tica</a></li>
+                            <li><a class="dropdown-item" href="#">R&eacute;gimen transitorio de asistencia</a></li>
+                            <li><a class="dropdown-item" href="#">Informaci&oacute;n sobre tr&aacute;mites en depto. de alumnos</a></li>
+                            <li><a class="dropdown-item" href="#">Video alumnos IES 1</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            DOCENTES
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Horas institucionales  </a></li>
+                            <li><a class="dropdown-item" href="#">Formularios  </a></li>
+                            <li><a class="dropdown-item" href="#">Tutoriales aula virtual  </a></li>
+                            <li><a class="dropdown-item" href="#">Concursos    </a></li>
+                            <li><a class="dropdown-item" href="#">Reglamento selecci&oacute;n docente    </a></li>
+                            <li><a class="dropdown-item" href="#">Ddjj online   </a></li>
+                            <li><a class="dropdown-item" href="#">Licencias mia y otros    </a></li>
+                            <li><a class="dropdown-item" href="#">Tr&aacute;mites  </a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            GRADUADOS
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Incorporaci&oacute;n al padr&oacute;n</a></li>
+                            <li><a class="dropdown-item" href="#">Sitios de inter&eacute;s</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            INFORMACI&Oacute;N &Uacute;TIL
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Elecciones 2024</a></li>
+                            <li><a class="dropdown-item" href="#">&Aacute;rea ddhh e inclusi&oacute;n  </a></li>
+                            <li><a class="dropdown-item" href="#">Departamento de fonoaudiolog&iacute;a  </a></li>
+                            <li><a class="dropdown-item" href="#">Biblioteca   </a></li>
+                            <li><a class="dropdown-item" href="#">Bedel&iacute;a - horarios  </a></li>
+                            <li><a class="dropdown-item" href="#">Cooperadora  </a></li>
+                            <li><a class="dropdown-item" href="#">Reglamentos varios </a></li>
+                            <li><a class="dropdown-item" href="#">Seminarios y talleres </a></li>
+                            <li><a class="dropdown-item" href="#">Fotos del alicia  </a></li>
+                            <li><a class="dropdown-item" href="#">Extensi&oacute;n acad&eacute;mica y comunitaria  </a></li>
+                            <li><a class="dropdown-item" href="#">Divulgaci&oacute;n acad&eacute;mica  </a></li>
+                            <li><a class="dropdown-item" href="#">&Aacute;rea de g&eacute;nero </a></li>
+                            <li><a class="dropdown-item" href="#">Post&iacute;tulos</a></li>
+                            <li><a class="dropdown-item" href="#">Club de ciencias </a></li>
+                            <li><a class="dropdown-item" href="#">Efem&eacute;rides</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            CARRERAS
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Presentaci&oacute;n carreras </a></li>
+                            <li><a class="dropdown-item" href="#">Programas 2022  </a></li>
+                            <li><a class="dropdown-item" href="#">Programas 2023  </a></li>
+                            <li><a class="dropdown-item" href="#">Programas 2024    </a></li>
+                            <li><a class="dropdown-item" href="#">Profesorados   </a></li>
+                            <li><a class="dropdown-item" href="#">Programas 2023  </a></li>
+                            <li><a class="dropdown-item" href="#">Carrera de psicopedagog&iacute;a   </a></li>
+                            <li><a class="dropdown-item" href="#">Correlatividades</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link no-dropdown toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            AULAS VIRTUALES
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    
